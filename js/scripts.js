8dbe2f18868413d2f61001b281351fda8d73f952
Austin Cummings <skwerl_e@yahoo.com>
2017-08-24 15:39:41 -0700

    trying to push this

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,13 @@ name2=$("#name2").val();
 var player1;
 var player2;
 var activePlayer;
-function Player (name,symbol) {
+var value;
+
+
+function Player (name,symbol,value) {
   this.symbol = symbol;
   this.name = name;
+  this.value = value;
 }
 function switchem(player1,player2){
 if(activePlayer===player1){
@@ -16,11 +20,37 @@ if(activePlayer===player1){
 //console.log(activePlayer.symbol);
 return activePlayer;
 }
+winConditions = [a0,a1,a2],[b0,b1,b2],[c0,c1,c2],[a0,b0,c0],[a1,c1,b1],[a2,c2,b2],[a0,b1,c2],[c0,b1,a2];
 
-function ticTacClick(){
+// function winConditionChecker {
+// for()
+// ($(this).attr('value'));
+// }
+
+function getValues() {
+  var row='a';
+  for(i=0;i<=4;i++){
+    for(i=0;i<=3;i++) {
+      current=row.concat(i);
+      console.log("current as name"+current);
+      current=row[i];
+      current=$(current).attr('value');
+      console.log("current as value"+current);
+    } if(row==='a') {
+      row='b';
+    } else if(row==='b') {
+      row='c';
+    } else {
+      break;
+    }
+  }
+};
+function ticTacClick(){ //disable those already clicked
 switchem(player1,player2);
 $(this).append('<p>'+activePlayer.symbol+'</p>');
-
+this.setAttribute("value", activePlayer.value);
+console.log(parseInt($(this).attr('value')));
+getValues();
 };
 $(".col-md-4").on("click",ticTacClick);
 
@@ -29,9 +59,9 @@ $(document).ready(function() {
        event.preventDefault();
        name1=$("#name1").val();
        name2=$("#name2").val();
-       player1=new Player(name1,'x');
-       player2=new Player(name2,'o');
+       player1=new Player(name1,'x',5);
+       player2=new Player(name2,'o',3);
        activePlayer=player2;
-       console.log(activePlayer+"playerswitchingstuff");
+
 });
 });
