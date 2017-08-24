934593168be23540456cce24e5c2a2142ac1f1d1
Austin Cummings <skwerl_e@yahoo.com>
2017-08-24 12:03:16 -0700

    working game for 2 players, doesnt score yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,29 @@
 var board;
 name1=$("#name1").val();
 name2=$("#name2").val();
-
+var player1;
+var player2;
+var activePlayer;
 function Player (name,symbol) {
   this.symbol = symbol;
   this.name = name;
 }
+function switchem(player1,player2){
+if(activePlayer===player1){
+  activePlayer=player2
+} else {activePlayer=player1}
+//activePlayer = (activePlayer === 'player1') ? 'player2' : 'player1';
+//console.log(activePlayer.symbol);
+return activePlayer;
+}
 
 function ticTacClick(){
-  alert(this.id);
-  $(this).append('<p>'+player1.symbol+'</p>');
-// check active player
-// append value to this div
+switchem(player1,player2);
+$(this).append('<p>'+activePlayer.symbol+'</p>');
+
 };
-
 $(".col-md-4").on("click",ticTacClick);
-// var winConditions = [[0,1,2], [3,4,5], [6,7,8],
-//                        [0,3,6], [1,4,7], [2,5,8],
-//                        [0,4,8], [6,4,2]]
 
-
-
-// below window refers to global, square brackets indicate that activePlayer is a variable
- $("#"+activePlayer+"-score").text(window[activePlayer].name);
-
-activePlayer = (activePlayer === 'player1') ? 'player2' : 'player1';
-// ternary - (condition) ? conditionTrueCase : conditionFalseCase;
-//ie... whoactive = if(activePlayer is player1) ? 'then' : 'else';
-
-//
-// $('#'+ m).click(function(){
-// 						turnCount += 1;
-// 						if (board[k][l] === "" && !gameOver) {
-// 							board[k][l] = currPlayer;
-// 							$('#' + m).append('<p>'+currPlayer+'</p>');
-// 							checkForWin();
-// 							if(currPlayer === "X") {
-// 								currPlayer = "O";
-// 							} else {
-// 								currPlayer = "X";
-// 							};
 $(document).ready(function() {
   $('#players').submit(function() {
        event.preventDefault();
@@ -47,7 +31,7 @@ $(document).ready(function() {
        name2=$("#name2").val();
        player1=new Player(name1,'x');
        player2=new Player(name2,'o');
-
-console.log(player1);
+       activePlayer=player2;
+       console.log(activePlayer+"playerswitchingstuff");
 });
 });
